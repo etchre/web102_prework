55ef8c6d6f64cf6ef615ba5a1027029718d11246
ethan <etchre@outlook.com>
2024-08-18 12:33:54 -0700

    implemented filters, and made filter buttons functional

--- index.js
+++ index.js
@@ -99,10 +99,15 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    let filtered_list = GAMES_JSON.filter((game) => {
+        if(game.pledged < game.goal) {
+            return true;
+        } 
+        return false;
+    });
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(filtered_list);
 }
 
 // show only games that are fully funded
@@ -110,10 +115,15 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
+    let filtered_list = GAMES_JSON.filter((game) => {
+        if(game.pledged >= game.goal) {
+            return true;
+        } 
+        return false;
+    });
 
-
-    // use the function we previously created to add unfunded games to the DOM
-
+    // use the function we previously created to add the unfunded games to the DOM
+    addGamesToPage(filtered_list);
 }
 
 // show all games
@@ -121,13 +131,27 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON)
 }
 
 // select each button in the "Our Games" section
 const unfundedBtn = document.getElementById("unfunded-btn");
+unfundedBtn.addEventListener(
+    'click',
+    filterUnfundedOnly
+);
+
 const fundedBtn = document.getElementById("funded-btn");
+fundedBtn.addEventListener(
+    'click',
+    filterFundedOnly
+)
+
 const allBtn = document.getElementById("all-btn");
+allBtn.addEventListener(
+    'click',
+    showAllGames
+)
 
 // add event listeners with the correct functions to each button
 
